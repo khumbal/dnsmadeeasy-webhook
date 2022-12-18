c006128538ab782c72e8b53d44c676748b94e261
Dmitry Lomanchuk <daloman@gmail.com>
2022-12-18 12:45:30 +0300

    Rename variable

--- src/main.go
+++ src/main.go
@@ -335,12 +335,12 @@ func findTxtRecord(client *GoDNSMadeEasy.GoDMEConfig, domainID int, zone, fqdn s
 	for _, existingRecord := range records {
 		// Remove quotas from Record.Value
 		truncateStr := "\""
-		recordValue := strings.TrimPrefix(existingRecord.Value, truncateStr)
-		recordValue = strings.TrimSuffix(recordValue, truncateStr)
+		unquotedRecordValue := strings.TrimPrefix(existingRecord.Value, truncateStr)
+		unquotedRecordValue = strings.TrimSuffix(unquotedRecordValue, truncateStr)
 
 		if existingRecord.Name == name &&
 			existingRecord.Type == "TXT" &&
-			recordValue == key {
+			unquotedRecordValue == key {
 			fmt.Printf("DNS record found: %v\n", existingRecord)
 			return &existingRecord
 		}
